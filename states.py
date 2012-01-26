c3aa0a60fb5326d473183e4c7fe4ef6cf6c93697
Jeremy Mikkola <jeremy128@gmail.com>
2012-01-26 11:36:11 -0500

    Added start of test case, fixed typos

--- states.py
+++ states.py
@@ -38,7 +38,7 @@ class Graph:
         self.states = dict()
         self._noIssued = 0
         
-    def addState(text):
+    def addState(self, text):
         ''' Adds a State object with the given text to the 
         graph, and returns the state number at which it is stored '''
         stateNo = self._noIssued
@@ -46,22 +46,32 @@ class Graph:
         self.states[stateNo] = State(stateNo, text)
         return stateNo
 
-    def removeState(num):
+    def removeState(self, num):
         ''' Removes a state (by number) from the graph '''
         del self.state[num]
         for n, state in self.states.iteritems():
             state.removeConnections(num)
 
-    def addTransition(state1, state2, command):
+    def addTransition(self, state1, state2, command):
         ''' Adds a transition from the state with number 
         state1 to the state with number state2 '''
         s1 = self.states[state1]
         s2 = self.states[state2]
         s1.addTransition(command, s2)
         
-    def removeTransition(state1, command):
+    def removeTransition(self, state1, command):
         ''' Removes a transition starting at state1 with
         the given command '''
         s1 = self.states[state1]
         s1.removeTransition(command)
         
+
+if __name__ == '__main__':
+    g = Graph()
+    sn1 = g.addState('first state')
+    sn2 = g.addState('another state')
+    sn3 = g.addState('third state')
+    g.addTransition(sn1, sn2, 'go up')
+    g.addTransition(sn1, sn3, 'pass')
+    g.addTransition(sn2, sn1, 'go down')
+    g.addTransition(sn3, sn1, 'back')
